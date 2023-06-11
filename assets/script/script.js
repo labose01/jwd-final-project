93a3a33021846d95316562c755c02c12e740ac76
labose01 <87338403+labose01@users.noreply.github.com>
2023-06-11 00:33:00 -0700

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,60 +1,14 @@
-const AddBtn =document.querySelector(".Add");
-const DeleteBtn =document.querySelector(".delete");
-const AddTaskInput=document.querySelector(".AddtaskInput")
-const tasks =document.querySelector(".tasks");
+const addBtn = document.querySelector(".add");
+const addTaskInput = document.querySelector(".addTaskInput");
+const tasks = document.querySelector(".task");
 
+const validFormFieldInput = () => {
 
-// functions
+    let data = addTaskInput.value;
 
-const  Addtask =()=>{
-
-    let AddTaskInputContent =AddTaskInput.value;
-
-
-    if(AddTaskInputContent==""){
-
-        alert("Please add a task name")
+    if(data == ""){
+        alert("Please add the task name")
         return;
     }
-   const task= document.createElement("div")
-   task.classList.add("task","border","d-flex","justify-content-between","mb-3");
-
-   const paragraph=document.createElement("p");
-   paragraph.classList.add("TaskContent","p-2");
-   paragraph.textContent=AddTaskInputContent;
-
-   task.appendChild(paragraph);
-
-   const span =document.createElement("span");
-   span.classList.add("bg-dark","p-3")
-   task.appendChild(span);
-
-   const a =document.createElement("a");
-   a.classList.add("text-decoration-none","text-white")
-   a.textContent="X";
-   a.href="#"
-   span.appendChild(a);
-
-   tasks.appendChild(task);
-   AddTaskInputContent="";
-
-
-
-   a.addEventListener("click",function(){
-
-
-    tasks.removeChild(task);
-
-
-
-   })
-
-}
-
-AddBtn.addEventListener("click",Addtask);
-
-DeleteBtn.addEventListener("click",function(){
-
-
-    tasks.textContent="";
-})
+} 
+addBtn.addEventListener("click", validFormFieldInput);
