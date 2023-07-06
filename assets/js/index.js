b1dc6135bc012f1b67bd54b5f12c5b7c05c16c58
Lidiya <aboselidiya@gmail.com>
2023-07-06 15:59:54 -0700

    Final for upload, save, delete button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
-//const taskManager = new TaskManager();
-const addBtn = document.querySelector(".add");
-const addTaskInput = document.querySelector("#inputField");
-// const taskDescription = document.querySelector(".description");
-// const taskAssigned = document.querySelector(".assignedTo");
-// const dueDate = document.querySelector(".date");
-//const task = document.querySelector(".task");
+const taskManager = new TaskManager();
+const addBtn = document.querySelector("#addTaskBtn");
+
+const taskForm = document.querySelector("#new-task-form");
+const deleteTask = document.getElementById("#deleteTaskBtn");
+
+
 
 
 const validFormFieldInput = (data) => {
 
-    
+    const addTaskInput = document.querySelector("#inputField");
     let newData = addTaskInput.value;
     if(newData == ""){
         alert("Please add the task name")
@@ -17,11 +17,83 @@ const validFormFieldInput = (data) => {
     }
 
 }
-
-
 addBtn.addEventListener("click", validFormFieldInput);
 
 
 
+document.getElementById('new-task-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const newTaskname = document.getElementById("inputField");
+    const newTaskdescription = document.getElementById("task-description");
+    const newTaskassignedTo = document.getElementById("task-assigned-to");
+    const newTaskType = document.getElementById("task-type");
+    const newTaskstatus = document.getElementById("task-status");
+    const newTaskdueDate = document.getElementById('task-due-date');
+
+    
+    //const saveButton = document.getElementById("saveTaskBtn");
+    
+    
+
+    //validation should be here
+
+    const name = newTaskname.value;
+    const description = newTaskdescription.value;
+    const assignedTo = newTaskassignedTo.value;
+    const taskType = newTaskType.value;
+    const status = newTaskstatus.value;
+    const dueDate = newTaskdueDate.value;
+    
+    taskManager.addTask(name, description, assignedTo, taskType, status, dueDate);
+    // save
+
+    taskManager.save();
+
+    //saveButton.addEventListener("click", () => submitForm("new"));
+
+    taskManager.render();
+
+    // Clear the form inputs
+    document.getElementById('inputField').value = '';
+    document.getElementById('task-description').value = '';
+    document.getElementById('task-assigned-to').value = '';
+    document.getElementById('task-type').value = '';
+    document.getElementById('task-status').value = '';
+    document.getElementById('task-due-date').value = '';
+    
+
+});
 
 
+//eventlisener done must use dom traversal
+
+// DOM Selector
+const tasksList = document.querySelector("#tasks-list")
+
+  
+// Event listener for the click event on the tasks list
+    tasksList.addEventListener('submit', (event) => {
+    if (event.target.classList.contains('done-button')) {
+          const parentTask = event.target.closest('.list-group-item');
+          const taskId = Number(parentTask.dataset.taskId);
+          taskManager.updateTaskStatus(taskId, 'DONE');
+          taskManager.save();
+          taskManager.render();
+        }
+    });
+  
+        // deleteTask.addEventListener('submit', () => {
+        //     tasksList.removeChild();
+        tasksList.addEventListener("click", (event) => {
+            if (event.target.classList.contains("delete-button")) {
+                const parentTask = event.target.parentElement.parentElement;
+                const taskId = Number(parentTask.dataset.taskId);
+                taskManager.deleteTask(taskId);
+            }
+        });
+    
+  
+  
+  // ...
+  
--- assets/js/taskManager.js
+++ assets/js/taskManager.js
@@ -1,23 +1,96 @@
-  class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
-    }   
+const createTaskHtml =(name, description, assignedTo, taskType, status, dueDate) => 
 
-    addTask(name, description, assignedTo, dueDate, status) {
-        this.currentId++;
+{
+    return `
+    <li class="list-group-item">
+      <div>
+        <div class="task">
+          <h5>Task Name: ${name}</h5>
+          <p>Task Description: ${description}</p>
+          <p>Assigned to: ${assignedTo}</p>
+          <p>Task Type: ${taskType}</p>
+          <p>Status: ${status}</p>
+          <p>Due date: ${dueDate}</p>
+          <button class="btn btn-sm btn-success done-button">Done</button>
+          <button class="btn btn-sm btn-danger delete-button">Delete</button>
+        </div>
+
+      <div>
+    </li>
+    `;
+};
+
+class TaskManager {
+  constructor(currentId = 1) {
+      this.tasks = [];
+      this.currentId = currentId;
+  }   
+
+    addTask(name, description, assignedTo, taskType, status, dueDate) {
+        // this.currentId++;
 
         //Push a new task into tasks array
         this.tasks.push({
-            id: this.currentId,
+            id: this.currentId++,
             name: name,
             description: description,
             assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: status
+            taskType: taskType,
+            status: status,
+            dueDate: dueDate
+            
             });
         
         }
+        updateTask(name, description, assignedTo, dueDate, status, taskType, taskId, taskPosition) {
+          const task = {
+              id: taskId,
+              name: name,
+              description: description,
+              assignedTo: assignedTo,
+              taskType: taskType,
+              dueDate: dueDate,
+              status: status,
+            };
+        
+            this.tasks[taskPosition] = task;
+      }
+
+      updateTaskStatus(taskId, newStatus) {
+        const task = this.tasks.find((task) => task.id === taskId);
+        if (task) {
+          task.status = newStatus;
+        }
+      }
+  
+    
+      // Method to delete a task
+  deleteTask(taskId) {
+    //this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    let result = window.confirm("Do you want to delete this task?")
+    // Create an empty array to store the new tasks
+    const newTasks = [];
+
+    if(result) {
+
+      // Loop over the tasks
+      for (let i = 0; i < this.tasks.length; i++) {
+        // Get the current task
+        const task = this.tasks[i];
+
+        // Check if the task id is not equal to the taskId to be deleted
+        if (task.id !== taskId) {
+          // Push the task into the newTasks array
+          newTasks.push(task);
+        }
+      
+      }
+    }
+    // Set this.tasks to the newTasks array
+    this.tasks = newTasks;
+    // console.log(newTasks);
+  }
+  
     
 
     //calling render
@@ -25,70 +98,63 @@
         let tasksHtmlList = [];
         for(let i = 0; i < this.tasks.length; i++){
             let currentTask = this.tasks[i];
-            let date = new Date(currentTask.dueDate);
+            // let date = new Date(currentTask.dueDate);
 
-            let formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+            // let formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 
             let taskHtml = createTaskHtml(
               currentTask.name,
               currentTask.description,
               currentTask.assignedTo,
-              formattedDate,
-              currentTask.status
+              currentTask.taskType,
+              currentTask.status,
+              currentTask.dueDate 
             );
 
             tasksHtmlList.push(taskHtml);
 
         }
-       let tasksHtml = tasksHtmlList.join('\n')
+       let tasksHtml = tasksHtmlList.join('\n');
        
        document.getElementById('tasks-list').innerHTML = tasksHtml;
     }
 
+
+  // Method to save tasks to localStorage
+  save() {
+    // Convert tasks array to JSON string
+    const tasksJson = JSON.stringify(this.tasks);
+    // Store the JSON string in localStorage
+    localStorage.setItem('tasks', tasksJson);
+
+    // Convert currentId to string
+    const currentIdString = String(this.currentId);
+    // Store currentId in localStorage
+    localStorage.setItem('currentId', currentIdString);
+  }
+
+  // Method to load tasks from localStorage
+  load() {
+    // Check if tasks are stored in localStorage
+    if (localStorage.getItem('tasks')) {
+      // Get the JSON string of tasks from localStorage
+      const tasksJson = localStorage.getItem('tasks');
+      // Convert the JSON string to an array and store it in this.tasks
+      this.tasks = JSON.parse(tasksJson);
+    }
+
+    // Check if currentId is stored in localStorage
+    if (localStorage.getItem('currentId')) {
+      // Get the currentId from localStorage
+      const currentIdString = localStorage.getItem('currentId');
+      // Convert the currentId to a number and store it in this.currentId
+      this.currentId = Number(currentIdString);
+    }
+  }
+
+
 }
 
-const createTaskHtml =(name, description, assignedTo, dueDate, status) => 
-
-{
-    return `
-    <li class="list-group-item">
-      <div class="task">
-        <h5>Task Name: ${name}</h5>
-        <p>Task Description: ${description}</p>
-        <p>Assigned to: ${assignedTo}</p>
-        <p>Due date: ${dueDate}</p>
-        <p>Status: ${status}</p>
-      </div>
-    </li>
-    `;
-};
-
-
-const taskManager = new TaskManager();
-
-document.getElementById('new-task-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const name = document.querySelector("inputField");
-    const description = document.querySelector("task-description");
-    const assignedTo = document.querySelector("task-assigned-to");
-    const dueDate = document.querySelector("task-due-date");
-    const status = document.getElementById("task-status");
-
-    
-
-    taskManager.addTask(name, description, assignedTo, dueDate, status);
-
-    // Clear the form inputs
-    document.getElementById('inputField').value = '';
-    document.getElementById('task-description').value = '';
-    document.getElementById('task-assigned-to').value = '';
-    document.getElementById('task-due-date').value = '';
-    document.getElementById('task-status').value = '';
-
-    taskManager.render();
-
-});
 
 
 
